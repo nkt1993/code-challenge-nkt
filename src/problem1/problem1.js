1876e50ba0be9934724d0d94bd4588925c9c9335
nkt <nkt@nkts-Air.homenet.telecomitalia.it>
2022-12-06 20:00:50 +0100

    problem1

--- src/problem1/problem1.js
+++ src/problem1/problem1.js
@@ -1,11 +1,22 @@
+// O(1)
 var sum_to_n_a = function(n) {
-    // your code here
+    return (n*(n+1))/2;
 };
 
+// O(n) iterative
 var sum_to_n_b = function(n) {
-    // your code here
+    let sum = 0;
+    for (let i = 1; i <= n; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
+// O(n) receursive
 var sum_to_n_c = function(n) {
-    // your code here
+    if (n == 0) {
+        return 0;
+    } else {
+        return n + sum_to_n_c(n-1);
+    }
 };
